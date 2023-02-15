053ed9c608d6ce2089cc5b8ef89d50bc7e9fbb3b
Zobelli <bozelli222gabriel@gmail.com>
2023-02-15 15:46:22 -0300

    Added Java classes to improve the abstraction

--- calculadora.java
+++ calculadora.java
@@ -1,6 +1,22 @@
+
+import java.util.Scanner;
+
 class program{
-    public static void main(String[] args) {
-        
+    public static void main(String[] args){
+        calculator a = new calculator();
+        a.run();
     }
 }
 
+class calculator{
+    Scanner keyboard = new Scanner(System.in);
+    double memory[]={};
+    int i = 0;
+    void run(){
+        String text = keyboard.nextLine();
+        double value = inter.eval(text);
+        System.out.println(value);
+        memory[i] = value;
+        i++;
+    }
+}
