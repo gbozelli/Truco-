2126cc94f354d0cf7d61c1db580941a48544ef6b
Zobelli <bozelli222gabriel@gmail.com>
2023-02-20 09:18:03 -0300

    modified:   frame.java

--- frame.java
+++ frame.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class frame extends JFrame{
     JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b0,
     bp,bm,bv,bd,
-    be,bb;
+    be,bb,bs;
     frame(){
         b1 = new JButton();b2 = new JButton();b3 = new JButton();b4 = new JButton();
         b5 = new JButton();b6 = new JButton();b7 = new JButton();b8 = new JButton();
